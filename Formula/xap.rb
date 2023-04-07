db12aadd2c442fe0d64a1664baa9f11fef94bb2f
jreleaserbot <jreleaser@kordamp.org>
2023-04-07 15:14:06 +0800

    xap 4.0.0-Alpha1

---
+++ Formula/xap.rb
@@ -0,0 +1,20 @@
+# Generated with JReleaser 1.5.1 at 2023-04-07T15:13:36.658753+08:00
+class Xap < Formula
+  desc "CLI for XPIE Application Platform"
+  homepage "https://www.xpie.cn"
+  url "https://github.com/chendouble/xap-releases/releases/download/4.0.0-Alpha1/xap-adminui-cli-app-4.0.0-Alpha1.zip"
+  version "4.0.0-Alpha1"
+  sha256 "dced21b7cd9358059a9fd36457f547965bf198d0eea26c41a3972bdae1a8ee3c"
+  license "Private"
+
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/xap" => "xap"
+  end
+
+  test do
+    output = shell_output("#{bin}/xap --version")
+    assert_match "4.0.0-Alpha1", output
+  end
+end
